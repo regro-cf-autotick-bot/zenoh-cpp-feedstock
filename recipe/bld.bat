674b45b7fe2ef029f44c14069a0ba51605d229dc
Silvio Traversaro <silvio@traversaro.it>
2024-10-23 22:21:19 +0200

    Build tests

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -24,6 +24,10 @@ if errorlevel 1 exit 1
 cmake --build . --config Release --target install
 if errorlevel 1 exit 1
 
+:: Build tests.
+cmake --build . --config Release --target tests
+if errorlevel 1 exit 1
+
 :: Test.
 ctest --output-on-failure -C Release
 if errorlevel 1 exit 1
