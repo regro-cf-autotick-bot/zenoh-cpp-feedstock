c2c6743f8dcdfde1979d6edfa521f9b76c2aed29
Silvio Traversaro <silvio@traversaro.it>
2024-10-23 19:38:40 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,11 +5,12 @@ rm -rf build
 mkdir build
 cd build
 
-# tests are disabled as of 0.11.0 they depend on zenohc's static library
-# switch them on on 1.0.0 as they can be compiled against zenohc's shared library
 cmake ${CMAKE_ARGS} -GNinja $SRC_DIR \
       -DCMAKE_BUILD_TYPE=Release \
-      -DBUILD_TESTING:BOOL=OFF
+      -DBUILD_TESTING:BOOL=ON \
+      -DZENOHCXX_ZENOHC:BOOL=ON \
+      -DZENOHCXX_ZENOHPICO:BOOL=OFF \
+      -DZENOHCXX_EXAMPLES_PROTOBUF:BOOL=OFF
 
 cmake --build . --config Release
 cmake --build . --config Release --target install
